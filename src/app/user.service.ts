7f4860d1f3786cb0e0c94fe14323c13534c67c4f
Dhvani Sheth <dhvanisheth25@gmail.com>
2023-11-02 13:42:26 -0400

    fix build error

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -8,6 +8,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
+  // private apiUrl = 'http://localhost:8081/api/v1/users';
   private apiUrl = 'https://nuestro.iverique.com/api/v1/users';
   constructor(private http: HttpClient, private authService: AuthenticationService) {}
   private getHeaders(): HttpHeaders {
@@ -21,4 +22,8 @@ export class UserService {
     const headers = this.getHeaders();
     return this.http.get(this.apiUrl+'/me', {headers});
   }
+
+  editDatabase(json: any): Observable<any> {
+    const headers = this.getHeaders();
+    return this.http.post(this.apiUrl+'/database',json, {headers});  }
 }
